acc3de3384ea5c86222dc77f38412cde8a4ce2d6
Hunter <khsagehorn@pbs.org>
2025-03-07 12:25:52 -0500

    add feature properties to features type

--- typings/Features.d.ts
+++ typings/Features.d.ts
@@ -17,6 +17,36 @@ type testOptions = {
     touch: boolean;
   }
 };
+
+type gameFeatures = {
+  buttonSize: boolean
+  captions: boolean
+  captionsStyles: boolean
+  colorVision: boolean
+  completionPercentage: boolean
+  controlSensitivity: boolean
+  dragThresholdScale: boolean
+  fullScreen: boolean
+  health: boolean
+  hitAreaScale: boolean
+  hudPosition: boolean
+  inputCount: boolean
+  keyBinding: boolean
+  music: boolean
+  musicVolume: boolean
+  objectCount: boolean
+  pointerSize: boolean
+  removableLayers: boolean
+  sfx: boolean
+  sfxVolume: boolean
+  sound: boolean
+  soundVolume: boolean
+  speedScale: boolean
+  timersScale: boolean
+  vo: boolean
+  voVolume: boolean
+}
+
 export class Features {
   static get basic(): string;
   static get canvas(): boolean;
@@ -28,4 +58,6 @@ export class Features {
   static get websockets(): boolean;
   static get webworkers(): boolean;
   static test(capabilities: testOptions): string | null;
+
+  data: gameFeatures;
 }
